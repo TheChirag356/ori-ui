39f6eb447ced9a92e822f0923b91fa970db5f6ad
TheChirag356 <arorachirag12005@gmail.com>
2025-12-09 16:07:31 +0530

    feat: footer

---
+++ src/lib/components/footer.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+</script>
+
+<footer class="text-center p-4 text-sm text-neutral-500">
+	Built by <a
+		href="https://x.com/chiragkun"
+		class="underline"
+		target="_blank"
+		rel="noopener noreferrer">Chirag</a
+	>. The source code is available on
+	<a
+		href="https://github.com/TheChirag356/ori-ui"
+		class="underline"
+		target="_blank"
+		rel="noopener noreferrer">GitHub</a
+	>.
+</footer>
