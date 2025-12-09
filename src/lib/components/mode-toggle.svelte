6d44ecbeb5949b6b843ce20eeba6105e77242e3f
TheChirag356 <arorachirag12005@gmail.com>
2025-12-09 16:07:53 +0530

    chore: added a title component to the mode-toggle

--- src/lib/components/mode-toggle.svelte
+++ src/lib/components/mode-toggle.svelte
@@ -6,7 +6,7 @@
 	import { Button } from '$lib/components/ui/button';
 </script>
 
-<Button onclick={toggleMode} variant="ghost" size="icon" class="cursor-pointer">
+<Button onclick={toggleMode} variant="ghost" size="icon" class="cursor-pointer" title="Toggle theme">
 	<SunIcon
 		class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all! dark:-rotate-90 dark:scale-0"
 	/>
