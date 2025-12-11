15141673efcaf9be08983d7ee185fd171e614d72
TheChirag356 <arorachirag12005@gmail.com>
2025-12-11 23:28:41 +0530

    fix: update footer styles to enhance link visibility

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -1,16 +1,16 @@
 <script lang="ts">
 </script>
 
-<footer class="text-center p-4 text-sm text-neutral-500">
+<footer class="text-center p-4 text-sm">
 	Built by <a
 		href="https://x.com/chiragkun"
-		class="underline"
+		class="underline text-primary"
 		target="_blank"
 		rel="noopener noreferrer">Chirag</a
 	>. The source code is available on
 	<a
 		href="https://github.com/TheChirag356/ori-ui"
-		class="underline"
+		class="underline text-primary"
 		target="_blank"
 		rel="noopener noreferrer">GitHub</a
 	>.
