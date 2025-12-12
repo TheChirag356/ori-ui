352bfd0183549de54604c69670595a6979c272e4
TheChirag356 <arorachirag12005@gmail.com>
2025-12-12 16:50:23 +0530

    fix: remove code-block component and add code-preview component

---
+++ src/lib/components/code-preview.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    let {children} = $props();
+</script>
+
+<div class="w-full min-h-56 border border-border flex justify-center items-center rounded-md p-4">
+	{@render children?.()}
+</div>
