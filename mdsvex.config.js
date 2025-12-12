4f0d385afbbaecf58ceedd151eaa748767de15ba
TheChirag356 <arorachirag12005@gmail.com>
2025-12-12 16:48:29 +0530

    fix: refactor highlighter function to use bundled languages and themes

--- mdsvex.config.js
+++ mdsvex.config.js
@@ -1,5 +1,5 @@
 import { escapeSvelte } from "mdsvex";
-import { createHighlighter } from "shiki"
+import { bundledLanguages, bundledThemes, createHighlighter } from "shiki"
 import remarkToc from "remark-toc";
 import rehypeSlug from "rehype-slug";
 
@@ -10,6 +10,19 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const path_to_layout = join(__dirname, './src/content/mdsvex.svelte');
 
+// use the single highlighter instance for better performance
+const shiki = await createHighlighter({
+    themes: Object.keys(bundledThemes),
+    langs: Object.keys(bundledLanguages),
+})
+
+async function highlighter(code, lang = "javascript") {
+    const html = escapeSvelte(shiki.codeToHtml(code, { lang, theme: 'vitesse-dark' }))
+    return `{@html \`
+    <div class="shiki">${html}</div>
+    \` }`
+}
+
 /** @type {import('mdsvex').MdsvexOptions} */
 export const mdsvexOptions = {
     extensions: [".md"],
@@ -17,14 +30,7 @@ export const mdsvexOptions = {
         _: path_to_layout
     },
     highlight: {
-        highlighter: async (code, lang = 'text') => {
-            const highlighter = await createHighlighter({
-                themes: ['poimandres'],
-                langs: ['javascript', 'typescript', 'go', 'rust', 'c', 'python', 'cpp', 'bash', 'svelte'],
-            })
-            const html = escapeSvelte(highlighter.codeToHtml(code, { lang, theme: 'poimandres' }))
-            return `{@html \`${html}\` }`
-        }
+        highlighter
     },
     remarkPlugins: [remarkToc],
     rehypePlugins: [rehypeSlug],
