0b03292a01337c56558d6ab57446a5132ce0f44d
TheChirag356 <arorachirag12005@gmail.com>
2025-12-22 13:59:12 +0530

    fix: update heading font style for consistency across components

--- src/routes/(main)/docs/[slug]/+page.svelte
+++ src/routes/(main)/docs/[slug]/+page.svelte
@@ -12,7 +12,7 @@
 {#key data?.meta.title}
 	<article class="max-w-3xl mx-auto">
 		<hgroup class="mt-8">
-			<h1 class="text-5xl font-semibold">{data?.meta.title}</h1>
+			<h1 class="text-5xl font-sans font-semibold">{data?.meta.title}</h1>
 			{#if data?.meta.description}
 				<p class="text-lg mb-4 mt-1 text-foreground/50 font-serif">{data?.meta.description}</p>
 			{/if}
