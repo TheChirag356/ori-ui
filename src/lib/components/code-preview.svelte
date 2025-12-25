ae7d99c10494aa561ff6e6cf87ebac47b18f0b26
TheChirag356 <arorachirag12005@gmail.com>
2025-12-25 23:20:00 +0530

    feat: add reload button to CodePreview component for dynamic content refresh

--- src/lib/components/code-preview.svelte
+++ src/lib/components/code-preview.svelte
@@ -1,13 +1,32 @@
 <script lang="ts">
-    import { cn } from "$lib/utils";
-    let {children, class: className} = $props();
+	import { cn } from "$lib/utils";
+	import { IconReload } from "@tabler/icons-svelte";
+	let { children, class: className } = $props();
+
+	let reloadKey = $state(0);
+
+	function reload() {
+		reloadKey++;
+	}
 </script>
 
 <div
 	class={cn(
-		'w-full min-h-56 border border-border flex justify-center items-center rounded-md p-4',
+		'relative w-full min-h-56 border border-border flex justify-center items-center rounded-md p-4',
 		className
 	)}
 >
-	{@render children?.()}
+	<!-- Reload Button -->
+	<button
+		type="button"
+		onclick={reload}
+		class="absolute top-2 cursor-pointer right-2 rounded-md border px-2 py-1 text-xs hover:bg-muted"
+	>
+		<IconReload class="size-5" />
+	</button>
+
+	<!-- Component remount -->
+	{#key reloadKey}
+		{@render children?.()}
+	{/key}
 </div>
