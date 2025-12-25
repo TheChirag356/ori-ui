66c98563b3217be373ece58df061fcba055c5509
TheChirag356 <arorachirag12005@gmail.com>
2025-12-25 14:06:37 +0530

    feat: update Morphic Navbar component to support array of nav items and improve active path handling

--- src/lib/registry/components/morphic-navbar/morphic-navbar.svelte
+++ src/lib/registry/components/morphic-navbar/morphic-navbar.svelte
@@ -1,6 +1,6 @@
 <script lang="ts" module>
     export type MorphicNavbarProps = {
-        navItems?: Record<string, { name: string }>;
+        navItems?: { href: string; name: string }[];
         class?: string;
     };
 </script>
@@ -8,48 +8,44 @@
 <script lang="ts">
 	import { cn } from "$lib/utils";
 
-    const items = {
-        "/": {
-            name: "home",
-        },
-        "/works": {
-            name: "works",
-        },
-        "/blog": {
-            name: "blog",
-        },
-        "/about": {
-            name: "about",
-        },
-    };
+    let items = [
+        { href: "#", name: "home" },
+        { href: "#works", name: "works" },
+        { href: "#blog", name: "blog" },
+        { href: "#about", name: "about" },
+    ] satisfies { href: string; name: string }[];
 
-    let activePath = $state("/");
+    let activePath = $state("#");
 
     function isActivePath(path: string) {
-        if (path === "/") {
-            return activePath === "/";
+        if (path === "#") {
+            return activePath === "#";
         }
         return activePath.startsWith(path);
     }
-
-    
+    let index = $state(0);
+    $effect(() => {
+        index = items.findIndex(item => isActivePath(item.href));
+    })
 
     let {navItems = items, class: className = ""}: MorphicNavbarProps = $props();
 </script>
 
 <nav class={cn('mx-auto max-w-4xl px-4 py-2', className)}>
 	<div class="flex items-center justify-center">
-		<div class="glass flex items-center justify-between overflow-hidden rounded-xl">
-			{#each Object.entries(navItems) as [path, item]}
+		<div class="glass flex items-center justify-between overflow-hidden rounded-xl transition-all">
+			{#each Object.entries(navItems) as [indx, { href, name }], idx (name)}
 				<a
 					class={cn(
 						'flex items-center justify-center bg-foreground p-1.5 px-4 text-sm text-background transition-all duration-300',
-						activePath === path ? 'mx-2 rounded-xl font-semibold text-sm' : cn()
+						activePath === href
+							? 'mx-2 rounded-xl font-semibold text-sm'
+							: cn(idx === index - 1 ? 'rounded-r-xl' : '', idx === index + 1 ? 'rounded-l-xl' : '')
 					)}
-					href="#"
+					{href}
 					onclick={() => {
-						activePath = path;
-					}}>{item.name}</a
+						activePath = href;
+					}}>{name}</a
 				>
 			{/each}
 		</div>
