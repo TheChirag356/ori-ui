1c880240a5e3e3b270c79e4855ba1d10dcee99d1
TheChirag356 <arorachirag12005@gmail.com>
2026-01-02 10:28:22 +0530

    fix: typo fix

--- src/lib/config/site.ts
+++ src/lib/config/site.ts
@@ -4,7 +4,7 @@ export const siteConfig = {
     description: "Free and open-source animated components and effects built with Svelte, Typescript, Tailwind CSS, and Motion. Perfect companion for svelte-shadcn/ui.",
     version: "1.0.0",
     ogImage: {
-        url: "https://oriui.app/assets/og-image.png",
+        url: `https://www.oriui.app/assets/ogimage.png`,
         height: 630,
         width: 1200,
         alt: "Ori UI - Animation-ready UI components for builders and designers. Built for Svelte"
