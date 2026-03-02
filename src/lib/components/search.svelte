7ce2f918bbc9c47efc61f91c2d9397f6c9114648
TheChirag356 <arorachirag12005@gmail.com>
2026-03-03 01:23:33 +0530

    search cmd hidden for smaller screens

--- src/lib/components/search.svelte
+++ src/lib/components/search.svelte
@@ -42,7 +42,7 @@
 
 <button 
   onclick={() => (open = true)}
-  class="relative inline-flex h-9 w-full items-center justify-start rounded-[0.5rem] border border-input bg-background px-4 py-2 text-sm font-medium text-muted-foreground shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 sm:pr-12 md:w-40 lg:w-64"
+  class="relative md:inline-flex h-9 w-full items-center justify-start rounded-[0.5rem] hidden border border-input bg-background px-4 py-2 text-sm font-medium text-muted-foreground shadow-sm transition-colors hover:bg-accent hover:text-accent-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 sm:pr-12 md:w-40 lg:w-64"
 >
   <span class="inline-flex">Search documentation...</span>
   <kbd class="pointer-events-none absolute right-1.5 top-1.5 hidden h-5 select-none items-center gap-1 rounded border bg-muted px-1.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
