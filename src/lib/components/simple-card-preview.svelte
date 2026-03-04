fa8836d868c4d455cd83de30214513ec7f0623c7
TheChirag356 <arorachirag12005@gmail.com>
2026-03-05 02:23:20 +0530

    registry files

--- src/lib/components/simple-card-preview.svelte
+++ src/lib/components/simple-card-preview.svelte
@@ -10,4 +10,14 @@
 			Card content goes here. You can add any elements you like!
 		</div>
 	{/snippet}
+	{#snippet body()}
+		<img
+			src="https://images.unsplash.com/photo-1768452523244-11bba3ab7d51?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+			alt="Simple Card Preview"
+			class="w-full h-40 object-cover rounded-md"
+		/>
+	{/snippet}
+	{#snippet headingBottom()}
+		<p class="text-sm text-gray-600 mb-4">Bottom Heading.</p>
+	{/snippet}
 </SimpleCard>
